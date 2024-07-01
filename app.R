faa316b5da80c0f92d571e9109715fd9f714210d
Matthew Gwynfryn Thomas <matthewgthomas@gmail.com>
2024-07-01 18:24:47 +0100

    Prepare to show election results

--- app.R
+++ app.R
@@ -1,11 +1,13 @@
 library(shiny)
 library(dplyr)
 library(readr)
-library(ggplot2)
 library(plotly)
+library(ggplot2)
 library(forcats)
 library(stringr)
 library(lubridate)
+library(yardstick)
+library(googlesheets4)
 
 # ---- Load data ----
 forecasts <- read_csv("forecasts.csv")
@@ -24,6 +26,46 @@ con_forecasts <-
   recent_forecasts |> 
   filter(Party == "Con")
 
+# Try to load election results
+gs4_deauth()
+election_results <- read_sheet("https://docs.google.com/spreadsheets/d/1R1c7oT6T8uCl8VlYhSIyjlLxy8_7J-p3voXcg-9T5fE/edit?gid=996500119#gid=996500119", sheet = "Results")
+
+show_election_results <- !is.na(election_results$`Actual seats`[1])
+
+# Which forecaster(s) were most accurate?
+if (show_election_results) {
+  forecasts_and_results <- 
+    recent_forecasts |> 
+    left_join(election_results) |> 
+    mutate(
+      Difference = abs(Seats - `Actual seats`)
+    )
+  
+  # Who was closest for Labour and Conservative?
+  lab_accuracy <- 
+    forecasts_and_results |> 
+    filter(Party == "Lab") |> 
+    filter(Difference == min(Difference, na.rm = TRUE)) |> 
+    distinct(Forecaster) |> 
+    pull(Forecaster)
+  
+  con_accuracy <- 
+    forecasts_and_results |> 
+    filter(Party == "Con") |> 
+    filter(Difference == min(Difference, na.rm = TRUE)) |> 
+    distinct(Forecaster) |> 
+    pull(Forecaster)
+  
+  # Who was closest overall, using mean absolute error?
+  overall_accuracy <- 
+    forecasts_and_results |> 
+    filter(Party %in% election_results$Party) |> 
+    group_by(Forecaster) |> 
+    mae(Seats, `Actual seats`) |> 
+    filter(.estimate == min(.estimate, na.rm = TRUE)) |> 
+    pull(Forecaster)
+}
+
 # Get dates
 election_date <- ymd("2024-07-04")
 current_date <- max(forecasts$Week)
@@ -31,10 +73,13 @@ current_date <- max(forecasts$Week)
 # ---- UI ----
 ui <- fluidPage(
     titlePanel("Seat Seer: Tracking seat predictions for the UK's General Election"),
+    
+    uiOutput("results"),
 
     h3("Every forecaster is predicting a Labour majority"),
     p(str_glue("As of {format(current_date, '%A %d %B %Y')}, Labour is projected to win anywhere from {min(lab_forecasts$Seats)} to {max(lab_forecasts$Seats)} seats. The Conservatives could win between {min(con_forecasts$Seats)} and {max(con_forecasts$Seats)} seats.")),
     p(str_glue("Combining these forecasts, Labour could win {round(mean(lab_forecasts$Seats), 0)} seats while the Conservatives could win {round(mean(con_forecasts$Seats), 0)}.")),
+    
     plotlyOutput("most_recent_forecasts"),
 
     h3("How have the forecasts changed over recent weeks?"),    
@@ -73,6 +118,18 @@ server <- function(input, output) {
         y = "Projected number of seats"
       )
     
+    # Include actual seats won, if known
+    if (show_election_results) {
+      plt <- 
+        plt +
+        geom_point(data = election_results, aes(x = Party, y = `Actual seats`, text = str_glue("{Party} won {`Actual seats`} seats.")), shape = 2, size = 2) +
+        theme(plot.title.position = "plot") +
+        labs(
+          title = "Projected number of seats (dots) and actual seats won (triangles)",
+          y = "Number of seats"
+        )
+    }
+    
     ggplotly(plt, tooltip = "text")
   })
   
@@ -95,7 +152,24 @@ server <- function(input, output) {
         y = "Projected number of seats"
       )
     
+    if (show_election_results) {
+      plt <- 
+        plt +
+        geom_hline(data = election_results |> filter(Party == "Lab"), aes(yintercept = `Actual seats`), linetype = 2, colour = "#d50000") +
+        geom_hline(data = election_results |> filter(Party == "Con"), aes(yintercept = `Actual seats`), linetype = 2, colour = "#0087dc")
+    }
+    
     ggplotly(plt)
+  })
+  
+  output$results <- renderUI({
+    if (show_election_results) {
+      div(
+        h3(str_glue("{str_flatten_comma(overall_accuracy, ', and')} published the most accurate forecast(s) overall")),
+        p(str_glue("{str_flatten_comma(lab_accuracy, ', and')} most closely predicted Labour's seats.")),
+        p(str_glue("{str_flatten_comma(con_accuracy, ', and')} most closely predicted the number of Conservative seats."))
+      )
+    }
   })
 }
 
